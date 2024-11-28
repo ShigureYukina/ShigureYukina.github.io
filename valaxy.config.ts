3156c62913efb8fb3d4e6367f9f34ae6d39e8eaf
ShigureYukina <shigureyukinai@gmail.com>
2024-11-28 15:34:22 +0800

    Update valaxy.config.ts by Qexo

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -6,8 +6,8 @@ export default defineValaxyConfig<ThemeUserConfig>({
 
   themeConfig: {
     banner: {
-      title: '欢迎来到千崎志乃的个人博客',
-      motto: 'それが わたしのストーリー，いつまでも 笑う ひとひら',
+      title: 'WELCOME',
+      motto: '欢迎来到千崎志乃的个人博客',
       urls: [
         'https://w.wallhaven.cc/full/2y/wallhaven-2ymr79.png'
       ],
