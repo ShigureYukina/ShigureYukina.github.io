b415c9214adb63e7255b033c7553cac493b7d668
ShigureYukina <shigureyukinai@gmail.com>
2024-11-28 15:18:37 +0800

    Update valaxy.config.ts by Qexo

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -9,7 +9,6 @@ export default defineValaxyConfig<ThemeUserConfig>({
       title: 'Hello, sakura',
       motto: 'You got to put the past behind you before you can move on.',
       urls: [
-        'https://wrxinyue-images.s3.bitiful.net/wallpaper/Genshin Impact - Yae Miko (4) Cybust PC.mp4',
         'https://wrxinyue-images.s3.bitiful.net/pc-wallpaper/wallhaven-yxwy7k.jpg'
       ],
       style: 'filter-dot',
