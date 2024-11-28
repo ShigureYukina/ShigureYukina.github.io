4b29f4fb66d941cbbdff6bbac5f75d20b6ee42d4
ShigureYukina <shigureyukinai@gmail.com>
2024-11-28 20:41:47 +0800

    替换主题，填充社交账户

--- site.config.ts
+++ site.config.ts
@@ -3,44 +3,22 @@ import { defineSiteConfig } from 'valaxy'
 export default defineSiteConfig({
   url: 'https://valaxy.site/',
   lang: 'zh-CN',
-  title: 'Valaxy Theme Yun',
+  title: '米崎妙妙屋',
   author: {
     name: '千崎志乃',
   },
   description: '千崎志乃的个人博客.',
   social: [
-    {
-      name: '个人QQ 1220870371',
-      link: 'tencent://AddContact/?fromId=45&fromSubId=1&subcmd=all&uin=1220870371',
-      icon: 'i-ri-qq-line',
-      color: '#12B7F5',
-    },
-    {
-      name: 'GitHub',
-      link: 'https://github.com/ShigureYukina',
-      icon: 'i-ri-github-line',
-      color: '#FFFFFF',
-    },
-    {
-      name: '哔哩哔哩',
-      link: 'https://space.bilibili.com/32494859',
-      icon: 'i-ri-bilibili-line',
-      color: '#FF8EB3',
-    },
-    {
-      name: 'E-Mail',
-      link: 'shigureyuki@duck.com',
-      icon: 'i-ri-mail-line',
-      color: '#8E71C1',
-    },
+
   ],
 
   search: {
-    enable: false,
+    enable: true,
+    type: 'engine',
   },
 
   sponsor: {
-    enable: true,
+    enable: false,
     title: '感谢支持',
     methods: [
       {
--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,17 +1,69 @@
-import { defineValaxyConfig } from 'valaxy'
-import type { ThemeUserConfig } from 'valaxy-theme-sakura'
+import type {UserThemeConfig} from 'valaxy-theme-yun'
+import {defineValaxyConfig} from 'valaxy'
 
-export default defineValaxyConfig<ThemeUserConfig>({
-  theme: 'sakura',
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+
+  // site config see site.config.ts
+
+  theme: 'yun',
 
   themeConfig: {
     banner: {
-      title: 'WELCOME',
-      motto: '欢迎来到千崎志乃的个人博客',
-      urls: [
-        'https://w.wallhaven.cc/full/2y/wallhaven-2ymr79.png'
-      ],
-      style: 'filter-dot',
+      enable: true,
+      title: '米崎妙妙屋',
+    },
+
+    pages: [
+      {
+        name: '个人QQ',
+        url: 'tencent://AddContact/?fromId=45&fromSubId=1&subcmd=all&uin=1220870371',
+        icon: 'i-ri-qq-line',
+        color: '#12B7F5',
+      },
+      {
+        name: 'Twitter',
+        url: 'https://x.com/sweyyuki',
+        icon: 'i-ri-twitter-line',
+        color: '#1DA1F2',
+      },
+      {
+        name: 'GitHub',
+        url: 'https://github.com/ShigureYukina',
+        icon: 'i-ri-github-line',
+        color: '#FFFFFF',
+      },
+      {
+        name: '哔哩哔哩',
+        url: 'https://space.bilibili.com/32494859',
+        icon: 'i-ri-bilibili-line',
+        color: '#FF8EB3',
+      },
+      {
+        name: 'E-Mail',
+        url: 'mailto:shigureyuki@duck.com',
+        icon: 'i-ri-mail-line',
+        color: '#8E71C1',
+      },
+    ],
+
+
+    footer: {
+      since: 2024,
+      beian: {
+        enable: false,
+        icp: '苏ICP备17038157号',
+      },
     },
   },
+  unocss: {safelist},
+
 })
+
